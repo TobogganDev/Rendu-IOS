5d048f758d39a95115ab8dd82268de08f0a87ae1
TobogganDev <doretg.t@gmail.com>
2024-10-15 12:10:21 +0200

    Initial Commit

---
+++ cours-swift/cours_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  cours_swiftApp.swift
+//  cours-swift
+//
+//  Created by Thomas Doret-Gaïsset on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct cours_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
